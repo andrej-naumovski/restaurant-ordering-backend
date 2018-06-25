a3d319b8875b3d92a45f8e98522985d747c1db97
Andrej Naumovski <Andrej.Naumovski@netcetera.com>
2018-06-25 14:24:11 +0200

    feature/[#24] Add operation methods to Money domain model.

--- src/models/domain/Money.js
+++ src/models/domain/Money.js
@@ -1,16 +1,49 @@
+import money from 'money-math'
+
 export default class Money {
   constructor(value, currency) {
     this._value = value
     this._currency = currency
   }
 
-  //TODO andrej-naumovski 15.06.2018: Add methods for handling money here
+  get value() {
+    return this._value
+  }
+
+  get currency() {
+    return this._currency
+  }
+
+  set value(value) {
+    this._value = value
+  }
+
+  set currency(currency) {
+    this._currency = currency
+  }
+
+  static get ZERO() {
+    return Money.of('0.00', 'EUR')
+  }
+
+  static of(value, currency) {
+    return new Money(value, currency)
+  }
+
+  add(that) {
+    const newValue = money.add(this.value, that.value)
+    this.value = newValue
+  }
+
+  subtract(that) {
+    const newValue = money.subtract(this.value, that.value)
+    this.value = newValue
+  }
 
   toJSON() {
-    const { _value, _currency } = this
     return {
-      value: _value,
-      currency: _currency,
+      value: money.format(this.currency, this.value),
+      currency: this.currency,
     }
   }
 }
