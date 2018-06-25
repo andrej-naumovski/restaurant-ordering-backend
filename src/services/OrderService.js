a0695e70e986963a2d834cbd7b3f8690b97637d0
Andrej Naumovski <Andrej.Naumovski@netcetera.com>
2018-06-25 12:57:18 +0200

    feature/[#15] Added endpoint for retrieving an Order given the order ID and the restaurant ID.
    * Created a service function which calls Mongoose and returns the Order.
    * Created a controller endpoint which takes the order ID as a path variable and the restaurant ID as a query param and returns the order.
    * Added new routes for the orders.

---
+++ src/services/OrderService.js
@@ -0,0 +1,11 @@
+import Order from '../models/persistence/Order'
+
+const getOrderByIdAndRestaurant = async (id, restaurantId) =>
+    await Order.findOne({
+        _id: id,
+        restaurantId
+    })
+
+export default {
+    getOrderByIdAndRestaurant,
+}
